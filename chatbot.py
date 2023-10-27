9b2654c5d98522f21b9bbf566c28edb864fc2ed4
saket <saketthakur4797@gmail.com>
2023-10-27 15:57:36 +0530

    stuff

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-from bot_response import patterns
+from bot_response import *
 import random
 import spacy
 nlp = spacy.load("en_core_web_lg")
@@ -24,8 +24,45 @@ def compare(sent1, sent2):
 
 didnt_get = {"4" : "I couldn't understand that, can you please rephrase your question?", "5" :"couldn't understand what you are saying.", "5.5" : "Can you make it more clear?"}
 
-
 # def survey_time():
+#     pass
+def get_options(QA):
+    QA = QA.split('\n')
+    the_question = QA[0]
+    options = [_ for _ in QA[1:] if _ != '' and _ !="Please Select"]
+    if len(options) == 0: options=None
+    return the_question, options
+
+def survey_time():
+    englih_survay = open('English QA.txt', 'r').read()
+    questions = englih_survay.split('//-')[1:]
+    for question in questions[11:]:
+        questions = get_options(question)
+        print(questions[0], '\n')
+        for _ in questions[1]:
+            print(_)
+        user_input = input("what option do you choose? : ")
+        similarity_score = []
+        for _ in questions[1]:
+            lamatized_user_input = lemmatize(user_input)
+            # lamatized_option = lemmatize(_)
+            # score = compare(lamatized_user_input, lamatized_option)
+            # similarity_score.append(score)
+            column = 0
+            for option_name in options_:
+                row = 0
+                for name in option_name:
+                    similarity_score.append(compare(lamatized_user_input, lemmatize(name)))
+                    row += 1
+                column += 1
+                # print(row)
+            print(column)
+        print(max(similarity_score), 'this is the max')
+        index = similarity_score.index(max(similarity_score))
+        print("you have selected",questions[1][index])
+    # print(similarity_score)
+
+
 
 #get the best response for a user input
 def get_response(user_input):

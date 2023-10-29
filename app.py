a511deb14df6d0cb36da1a57de99491bcf7aef08
saket <saketthakur4797@gmail.com>
2023-10-29 11:15:18 +0530

    v.3.5 app.py and html changed

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 from chatbot import get_response, survey_time
 
-app = Flask(__name__)
+app = Flask(__name)
 
 @app.route('/')
 def home():
     # Load the first survey question and options
     question_number = 2
     the_question, options, _ = survey_time(question_number, "")
-    return render_template('chatbot.html', question=the_question, options=options, question_number=question_number)
+    return render_template('chatbot.html', question=the_question, options=options)
 
 @app.route('/ask', methods=['POST'])
 def ask():
@@ -18,9 +18,9 @@ def ask():
 
 @app.route('/survey', methods=['POST'])
 def survey():
-    question_number = int(request.form['question_number'])
-    user_input = request.form['user_input']
-    selected_option, question_set, actual_question,  = survey_time(question_number, user_input)
+    question_number = int(request.json['question_number'])
+    user_input = request.json['user_input']
+    selected_option, question_set, actual_question = survey_time(question_number, user_input)
 
     # Load the next survey question and options
     next_question_number = question_number + 1

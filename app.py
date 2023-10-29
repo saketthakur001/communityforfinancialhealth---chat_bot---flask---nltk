dc36342806251e0c92ec6ab9b2ea79ba4e0e36d7
saket <saketthakur4797@gmail.com>
2023-10-29 10:53:01 +0530

    udpated html and app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template, request, jsonify
-from bot_response import get_options, lemmatize, compare
-import random
-import spacy
-import math
+from chatbot import get_response, survey_time
 
 app = Flask(__name__)
-nlp = spacy.load("en_core_web_lg")
-
-# Define the patterns, didn't_get, and other necessary functions here
 
 @app.route('/')
 def home():
     return render_template('chatbot.html')
 
-@app.route('/get_response', methods=['POST'])
-def get_bot_response():
+@app.route('/ask', methods=['POST'])
+def ask():
     user_input = request.form['user_input']
     response = get_response(user_input)
     return jsonify({'response': response})
+
+@app.route('/survey', methods=['POST'])
+def survey():
+    question_number = int(request.form['question_number'])
+    user_input = request.form['user_input']
+    selected_option, question_set, actual_question = survey_time(question_number, user_input)
+    return jsonify({'selected_option': selected_option, 'question_set': question_set, 'actual_question': actual_question})
 
 if __name__ == '__main__':
     app.run(debug=True)
